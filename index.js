8e752d834f5593a4f80d0941b6f3839d3a42fa6a
Shubham Phadte <sage@Shubhams-MacBook-Air.local>
2024-03-16 21:40:43 +0530

    Index js changes

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-Hi
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const dbConnection = require("./config/db");
+
+const urlController = require("./controllers/url.controller");
+
+const urlRouter = require("./routes/url.routes");
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: false }));
+app.use(bodyParser.json({ type: "application/json", limit: "50mb" }));
+
+dbConnection.dbConnect();
+
+app.get("/urls", (req, res) => {
+  res.send("Welcome to URL Shortner Application.");
+});
+
+app.use("/", urlRouter);
+
+app.listen(8000, () => {
+  console.log("everything ran succesfully.");
+});
